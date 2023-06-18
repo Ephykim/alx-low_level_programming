ed24c40331760535276eecc475dcc086ebcd9a4f
Ephy <ephyk48@gmail.com>
2023-06-18 10:52:10 -0700

    update 5-prinf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
-  * main-Entry point of program
-  * Description-using prinf function
-  *
-  * Return:(0) if success
-  */
+ * main-a program that uses printf to display characters
+ * description-using printf instead of puts
+ * Return:0 (success)
+ */
 int main(void)
-{ 
-	prinf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
