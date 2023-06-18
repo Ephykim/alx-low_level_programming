10b707d05a1320fa89d3023b6184d4834863afed
Ephy <ephyk48@gmail.com>
2023-06-18 10:41:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+  * main-Entry point of program
+  * Description-using prinf function
+  *
+  * Return:(0) if success
+  */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
