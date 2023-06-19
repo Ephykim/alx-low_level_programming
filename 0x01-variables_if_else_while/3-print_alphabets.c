8cce452ea6baf103ec0c3469e7b8922c03c02685
Ephy <ephyk48@gmail.com>
2023-06-19 09:59:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a function that printss out alphabets in caps and small letters
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int A = 65;
+	int a = 97;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 90)
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
