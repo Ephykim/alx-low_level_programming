911812ca4ec5faac929d448a0e359e93b8334306
Ephy <ephyk48@gmail.com>
2023-06-19 11:10:00 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+  * main-Prints all possible combination of single digits
+  * Return: (0)
+  */
+
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		putchar(i);
+		if (i <= 56)
+			putchar(',');
+			putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
