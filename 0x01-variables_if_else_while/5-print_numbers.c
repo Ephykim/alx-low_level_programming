65572fe38235f2f785173ad1345e5b3773f5b0ef
Ephy <ephyk48@gmail.com>
2023-06-19 10:28:24 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+  * main-Entry point of the program
+  * Return:0
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+
+	printf("\n");
+	return (0);
+}
