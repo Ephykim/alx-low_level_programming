7f66dd104bd2e01d98c6e905fd15136235640e09
Ephy <ephyk48@gmail.com>
2023-06-20 10:27:22 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *times_table-prints the timestable of
+ *Return:no return
+*/
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			_putchar(k + 48);
+			if (j == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
