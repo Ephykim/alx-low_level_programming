0bb8121cc7ebdec6e6a14aa68b030d8f8641ae4d
Ephy <ephyk48@gmail.com>
2023-06-24 00:05:30 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul-Multiplies two integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: answer
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
