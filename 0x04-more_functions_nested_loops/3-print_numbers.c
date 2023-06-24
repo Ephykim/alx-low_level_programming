df4bdb67f82b1346ebfa064e5f07b009f628a867
Ephy <ephyk48@gmail.com>
2023-06-24 00:13:05 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers-Print numbers
+  * Return: 0 when success
+  */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
