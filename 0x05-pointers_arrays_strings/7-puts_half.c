7331a60d6901616098b25c6b29b2be12c3c13cab
Ephy <ephyk48@gmail.com>
2023-06-26 10:27:32 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: String variable
+ */
+void puts_half(char *str)
+{
+	int length_of_the_string, n;
+
+	length_of_the_string = 0;
+	while (str[length_of_the_string] != '\0')
+	{
+		length_of_the_string++;
+	}
+	if (length_of_the_string % 2 != 0)
+	{
+		n = (length_of_the_string + 1) / 2;
+	}
+	else
+	{
+		n = length_of_the_string / 2;
+	}
+	while (n < length_of_the_string)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
