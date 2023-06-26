19b428469f4005e7883980c28fb8af53c6ff99df
Ephy <ephyk48@gmail.com>
2023-06-26 09:08:04 -0700

    Updated 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,22 @@ void puts_half(char *str)
 {
 	int i, j;
 
-	for (i = 0; str != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 	if (i % 2 != 0)
 	{
-		for (j = (i - 1) / 2; j <= i; j++)
+		for (j = (i - 1) / 2; j < i; j++)
+		{
 			_putchar(str[j]);
+		}
 		_putchar('\n');
 	}
 	else
 	{
-		for (j = i / 2; j <= i; j++)
+		for (j = i / 2; j < i; j++)
+		{
 			_putchar(str[j]);
+		}
 		_putchar('\n');
 	}
 }
