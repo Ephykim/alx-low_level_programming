e53e83a2f6e5bbdd77baf2676ed79a98c2611303
EPHY KIMOTHO <ephyk48@gmail.com>
2023-07-03 16:30:10 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,15 @@
 
 char *rot13(char *s)
 {
-	int i = 0;
+    int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	{
-		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-			s[i] += 13;
-		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-			s[i] -= 13;
-	}
-	return (s);
+    for (; s[i] != '\0'; i++)
+    {
+        if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+            s[i] += 13;
+        else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+            s[i] -= 13;
+    }
+
+    return s;
 }
