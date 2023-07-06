005c9d80179ba23af4badfb48a16ac9f58ba5242
Ephy <ephyk48@gmail.com>
2023-07-06 09:29:19 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
-  * _print_rev_recursion-Prints a string in reverse
-  * @s: pointer to string
-  * 
-  * Return: Void
-  */
+ * _print_rev_recursion-Prints a string in reverse
+ * @s: pointer to string
+ * Return: Void
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
