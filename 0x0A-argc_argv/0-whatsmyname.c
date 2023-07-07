ee8dfef14c026c7bf07b2003b5e54330cf820332
Ephy <ephyk48@gmail.com>
2023-07-07 12:54:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main-Prints the programs name
+* @argc: Number of arguments
+* @argv: array of arguments
+*
+* Return: (0);
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
