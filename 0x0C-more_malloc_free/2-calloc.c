f929513a9c7d41f9fc68e5a68d760d1fa5aa2226
Ephy <ephyk48@gmail.com>
2023-07-13 02:08:43 -0700

    Task 0, 1 and 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+  * _calloc-Allocates memory for an array
+  * @nmemb: Members of the array
+  * @size: Size of array
+  *
+  * Return: void pointer
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
