0a7f0a8a1d73e0af8d1c0faf177630719e63e125
Ephy <ephyk48@gmail.com>
2023-07-29 09:38:03 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * print_main-Prints string to the console
+  *
+  * Return: void
+  */
+void print_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
+
