e32aca2bb19eea01e146533567e5d51777e54100
Ephy <ephyk48@gmail.com>
2023-07-31 02:16:23 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * print_listint- prints all the elements of a listint_t list
+  * @h: pointer to a struct
+  *
+  * Return: Count
+  */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	size_t count;
+
+	current = h;
+	count = 0;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
